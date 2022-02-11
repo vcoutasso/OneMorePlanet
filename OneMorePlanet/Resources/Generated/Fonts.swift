6f7ed68f5220631ab1183ba9ce1021c0be957852
Ana Kessler <anakesslerg@gmail.com>
2022-02-11 11:09:30 -0300

    new tutorial with border

--- OneMorePlanet/Resources/Generated/Fonts.swift
+++ OneMorePlanet/Resources/Generated/Fonts.swift
@@ -2,9 +2,9 @@
 // Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
 
 #if os(macOS)
-    import AppKit.NSFont
+  import AppKit.NSFont
 #elseif os(iOS) || os(tvOS) || os(watchOS)
-    import UIKit.UIFont
+  import UIKit.UIFont
 #endif
 
 // Deprecated typealiases
@@ -18,76 +18,73 @@ internal typealias Font = FontConvertible.Font
 
 // swiftlint:disable identifier_name line_length type_body_length
 internal enum Fonts {
-    internal enum AldoTheApache {
-        internal static let regular = FontConvertible(name: "AldotheApache", family: "Aldo the Apache", path: "aldotheapache.ttf")
-        internal static let all: [FontConvertible] = [regular]
-    }
-
-    internal static let allCustomFonts: [FontConvertible] = [AldoTheApache.all].flatMap { $0 }
-    internal static func registerAllCustomFonts() {
-        allCustomFonts.forEach { $0.register() }
-    }
+  internal enum AldoTheApache {
+    internal static let regular = FontConvertible(name: "AldotheApache", family: "Aldo the Apache", path: "aldotheapache.ttf")
+    internal static let all: [FontConvertible] = [regular]
+  }
+  internal static let allCustomFonts: [FontConvertible] = [AldoTheApache.all].flatMap { $0 }
+  internal static func registerAllCustomFonts() {
+    allCustomFonts.forEach { $0.register() }
+  }
 }
-
 // swiftlint:enable identifier_name line_length type_body_length
 
 // MARK: - Implementation Details
 
 internal struct FontConvertible {
-    internal let name: String
-    internal let family: String
-    internal let path: String
+  internal let name: String
+  internal let family: String
+  internal let path: String
 
-    #if os(macOS)
-        internal typealias Font = NSFont
-    #elseif os(iOS) || os(tvOS) || os(watchOS)
-        internal typealias Font = UIFont
-    #endif
+  #if os(macOS)
+  internal typealias Font = NSFont
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  internal typealias Font = UIFont
+  #endif
 
-    internal func font(size: CGFloat) -> Font {
-        guard let font = Font(font: self, size: size) else {
-            fatalError("Unable to initialize font '\(name)' (\(family))")
-        }
-        return font
+  internal func font(size: CGFloat) -> Font {
+    guard let font = Font(font: self, size: size) else {
+      fatalError("Unable to initialize font '\(name)' (\(family))")
     }
+    return font
+  }
 
-    internal func register() {
-        // swiftlint:disable:next conditional_returns_on_newline
-        guard let url = url else { return }
-        CTFontManagerRegisterFontsForURL(url as CFURL, .process, nil)
-    }
+  internal func register() {
+    // swiftlint:disable:next conditional_returns_on_newline
+    guard let url = url else { return }
+    CTFontManagerRegisterFontsForURL(url as CFURL, .process, nil)
+  }
 
-    fileprivate var url: URL? {
-        // swiftlint:disable:next implicit_return
-        return BundleToken.bundle.url(forResource: path, withExtension: nil)
-    }
+  fileprivate var url: URL? {
+    // swiftlint:disable:next implicit_return
+    return BundleToken.bundle.url(forResource: path, withExtension: nil)
+  }
 }
 
 internal extension FontConvertible.Font {
-    convenience init?(font: FontConvertible, size: CGFloat) {
-        #if os(iOS) || os(tvOS) || os(watchOS)
-            if !UIFont.fontNames(forFamilyName: font.family).contains(font.name) {
-                font.register()
-            }
-        #elseif os(macOS)
-            if let url = font.url, CTFontManagerGetScopeForURL(url as CFURL) == .none {
-                font.register()
-            }
-        #endif
-
-        self.init(name: font.name, size: size)
+  convenience init?(font: FontConvertible, size: CGFloat) {
+    #if os(iOS) || os(tvOS) || os(watchOS)
+    if !UIFont.fontNames(forFamilyName: font.family).contains(font.name) {
+      font.register()
     }
+    #elseif os(macOS)
+    if let url = font.url, CTFontManagerGetScopeForURL(url as CFURL) == .none {
+      font.register()
+    }
+    #endif
+
+    self.init(name: font.name, size: size)
+  }
 }
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
-    static let bundle: Bundle = {
-        #if SWIFT_PACKAGE
-            return Bundle.module
-        #else
-            return Bundle(for: BundleToken.self)
-        #endif
-    }()
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
 }
-
 // swiftlint:enable convenience_type
--- OneMorePlanet/Sources/Game/Scenes/Menu/TutorialViewController.swift
+++ OneMorePlanet/Sources/Game/Scenes/Menu/TutorialViewController.swift
@@ -19,11 +19,25 @@ class TutorialViewController: UIViewController {
 
     private lazy var tutorial1: UIStackView = {
         let imageView = UIImageView(image: UIImage(asset: Assets.Images.tutorial1))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
         let label = UILabel()
         label.font = UIFont(font: Fonts.AldoTheApache.regular, size: 15)
-        label.text = "HOLD ANYWHERE"
+        label.text = "TAP NEAR THE PLANETS TO ACTIVATE\n THEIR GRAVITATIONAL FIELDS"
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
         label.textColor = .white
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 6
+        
+        let attributedString = NSMutableAttributedString(string: "TAP NEAR THE PLANETS TO ACTIVATE\n THEIR GRAVITATIONAL FIELDS")
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+        
+        label.attributedText = attributedString
         label.textAlignment = .center
+        
         let stack = UIStackView(arrangedSubviews: [
             imageView,
             label,
@@ -31,42 +45,61 @@ class TutorialViewController: UIViewController {
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.spacing = 15
-
+        stack.distribution = .fillProportionally
+        stack.contentMode = .scaleAspectFit
+        stack.layer.borderWidth = 1
+        stack.layer.cornerRadius = 30
+        stack.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 100)
+        stack.layoutMargins = UIEdgeInsets(top: 0, left: 5, bottom: 5, right: 5)
+        stack.isLayoutMarginsRelativeArrangement = true
+        
+        NSLayoutConstraint.activate([
+            
+            label.widthAnchor.constraint(equalTo: imageView.widthAnchor)
+        ])
+        
         return stack
     }()
 
     private lazy var tutorial2: UIStackView = {
         let imageView = UIImageView(image: UIImage(asset: Assets.Images.tutorial2))
+        imageView.contentMode = .scaleAspectFit
+        
         let label = UILabel()
         label.font = UIFont(font: Fonts.AldoTheApache.regular, size: 15)
-        label.text = "USE THE GRAVITATIONAL ORBITS"
+        label.text = "DON'T HIT THE PLANETS OR\n ASTEROIDS DIRECTLY"
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
         label.textColor = .white
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = 6
+        
+        let attributedString = NSMutableAttributedString(string: "DON'T HIT THE PLANETS OR\n ASTEROIDS DIRECTLY")
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+        
+        label.attributedText = attributedString
         label.textAlignment = .center
+        
         let stack = UIStackView(arrangedSubviews: [
             imageView,
-            label,
+            label
         ])
         stack.translatesAutoresizingMaskIntoConstraints = false
         stack.axis = .vertical
         stack.spacing = 15
-
-        return stack
-    }()
-
-    private lazy var tutorial3: UIStackView = {
-        let imageView = UIImageView(image: UIImage(asset: Assets.Images.tutorial3))
-        let label = UILabel()
-        label.font = UIFont(font: Fonts.AldoTheApache.regular, size: 15)
-        label.text = "DON'T HIT THE PLANET DIRECTLY"
-        label.textColor = .white
-        label.textAlignment = .center
-        let stack = UIStackView(arrangedSubviews: [
-            imageView,
-            label,
+        stack.distribution = .fillProportionally
+        stack.contentMode = .scaleAspectFit
+        stack.layer.borderWidth = 1
+        stack.layer.cornerRadius = 30
+        stack.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 100)
+        stack.layoutMargins = UIEdgeInsets(top: 0, left: 12, bottom: 5, right: 12)
+        stack.isLayoutMarginsRelativeArrangement = true
+        
+        NSLayoutConstraint.activate([
+            
+            label.widthAnchor.constraint(equalTo: imageView.widthAnchor)
         ])
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .vertical
-        stack.spacing = 15
 
         return stack
     }()
@@ -78,6 +111,7 @@ class TutorialViewController: UIViewController {
         let configuration = UIImage.SymbolConfiguration(pointSize: 38.0, weight: .medium)
         button.setPreferredSymbolConfiguration(configuration, forImageIn: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = UIColor(asset: Assets.Colors.buttonBackground)
 
         return button
     }()
@@ -97,19 +131,15 @@ class TutorialViewController: UIViewController {
 
             tutorial1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             tutorial1.topAnchor.constraint(equalTo: titleStackView.bottomAnchor,
-                                           constant: 20),
-
+                                           constant: 30),
+            
             tutorial2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             tutorial2.topAnchor.constraint(equalTo: tutorial1.bottomAnchor,
                                            constant: LayoutMetrics.distanceBetweenImages),
-
-            tutorial3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            tutorial3.topAnchor.constraint(equalTo: tutorial2.bottomAnchor,
-                                           constant: LayoutMetrics.distanceBetweenImages),
-
+            
             checkButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             checkButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,
-                                                constant: -20),
+                                             constant: -30),
         ]
         NSLayoutConstraint.activate(constraints)
     }
@@ -122,7 +152,6 @@ class TutorialViewController: UIViewController {
     private func setupHierarchy() {
         view.addSubview(tutorial1)
         view.addSubview(tutorial2)
-        view.addSubview(tutorial3)
         view.addSubview(titleStackView)
         view.addSubview(checkButton)
     }
@@ -130,7 +159,8 @@ class TutorialViewController: UIViewController {
     private enum LayoutMetrics {
         static let titleFontSize: CGFloat = 35
         static let titleStackViewSpacing: CGFloat = 30
-        static let distanceBetweenImages: CGFloat = 20
+        static let distanceBetweenImages: CGFloat = 30
+        
     }
 
     @objc private func checkButtonTapped() {
